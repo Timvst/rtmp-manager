645c406dbe643319faafa79aecc266d6fb6ccd47
Tim van Steenbergen <t.v.steenbergen@gmail.com>
2017-10-31 21:25:28 +0100

    yarn init

--- app.js
+++ app.js
@@ -136,10 +136,10 @@ app.get('/account/unlink/:provider', passportConfig.isAuthenticated, userControl
 /**
  * OAuth authentication routes. (Sign in)
  */
- app.get('/auth/instagram', passport.authenticate('instagram'));
-app.get('/auth/instagram/callback', passport.authenticate('instagram', { failureRedirect: '/login' }), (req, res) => {
-  res.redirect(req.session.returnTo || '/');
-});
+// app.get('/auth/instagram', passport.authenticate('instagram'));
+// app.get('/auth/instagram/callback', passport.authenticate('instagram', { failureRedirect: '/login' }), (req, res) => {
+//   res.redirect(req.session.returnTo || '/');
+// });
 app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email', 'public_profile'] }));
 app.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/login' }), (req, res) => {
   res.redirect(req.session.returnTo || '/');
